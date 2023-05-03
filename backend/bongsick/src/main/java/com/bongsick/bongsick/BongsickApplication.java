e722cfa6f4d52ba9e721c424245ce771a6cc9140
Jaewon Chung <jaewonchung97@gmail.com>
2023-05-03 15:21:44 +0900

    Initialized

---
+++ backend/bongsick/src/main/java/com/bongsick/bongsick/BongsickApplication.java
@@ -0,0 +1,13 @@
+package com.bongsick.bongsick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BongsickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BongsickApplication.class, args);
+    }
+
+}
---
+++ backend/bongsick/src/test/java/com/bongsick/bongsick/BongsickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bongsick.bongsick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BongsickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
